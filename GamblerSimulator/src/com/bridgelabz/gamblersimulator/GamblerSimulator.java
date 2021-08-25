034758467ee8505745335769365dd6218fd41759
Arpitha Bhat <arpithabht12@gmail.com>
2021-08-25 20:09:40 +0530

    REFACTOR: For readability changed naming convention

--- GamblerSimulator/src/com/bridgelabz/gamblersimulator/GamblerSimulator.java
+++ GamblerSimulator/src/com/bridgelabz/gamblersimulator/GamblerSimulator.java
@@ -1,8 +1,8 @@
 package com.bridgelabz.gamblersimulator;
 
 public class GamblerSimulator {
-	public static final int INITIAL_STAKE = 100;
-	public static final int BET_VALUE = 1;
+	public static final int TOTAL_STAKE = 100;
+	public static final int BET_PER_GAME = 1;
 	public static void main(String[] args) {
 		System.out.println("Welcome to gambler simulator");
 
