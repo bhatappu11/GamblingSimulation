6551ff87c927dcde6a1222939e6cb3597396dc08
Arpitha Bhat <arpithabht12@gmail.com>
2021-08-25 07:13:52 +0530

    ADDED: greetings are added

---
+++ GamblerSimulator/src/com/bridgelabz/gamblersimulator/GamblerSimulator.java
@@ -0,0 +1,10 @@
+package com.bridgelabz.gamblersimulator;
+
+public class GamblerSimulator {
+
+	public static void main(String[] args) {
+		System.out.println("Welcome to gambler simulator");
+
+	}
+
+}
