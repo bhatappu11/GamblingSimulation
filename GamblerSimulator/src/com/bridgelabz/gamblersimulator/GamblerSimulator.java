59aefa83b8a02b9b21769a118f8f09be3ea92c09
Arpitha Bhat <arpithabht12@gmail.com>
2021-08-25 08:07:38 +0530

    ADDED: code to know won or lost by how much

--- GamblerSimulator/src/com/bridgelabz/gamblersimulator/GamblerSimulator.java
+++ GamblerSimulator/src/com/bridgelabz/gamblersimulator/GamblerSimulator.java
@@ -1,51 +1,78 @@
 package com.bridgelabz.gamblersimulator;
 
 public class GamblerSimulator {
-	public static final int INITIAL_STAKE = 100;
-	public static final int BET_VALUE = 1;
+	public static final int TOTAL_STAKE=100;
+	public static final int BET_PER_GAME=1;
 	
-	public static int playGame(int stake) {
-		double winProbability = Math.random();
-		if(winProbability>0.5) {
-			System.out.println("Player has won the bet");
-			return stake=stake+1;
-		}
-		else {
-			System.out.println("Player has lost the bet");
-			return stake=stake-1;
-		}
-			
+	public static void main(String[] args)
+	{
+		int availableBalance=TOTAL_STAKE;
+		System.out.println("Starting Balance:"+availableBalance);
+		int noOfDays=20;
+		int totalAmount=0;
+		totalAmount=gamePlayForDays(noOfDays, availableBalance);
+		System.out.println("Amount won after "+ noOfDays+" days of play:"+totalAmount);
+		gameForMonth();
 	}
-	public static boolean terminalCondition(int stake) {
-		if(stake > (INITIAL_STAKE/2) || stake < (INITIAL_STAKE+(INITIAL_STAKE/2)))
-			return false;
-		else
-			return true;
+	public static int playGame(int availableBalance)
+	{
+		double wonOrLost=Math.random();
+		if(wonOrLost>0.5)
+		{
+			return availableBalance+BET_PER_GAME;
+		}
+		else 
+		{
+			return availableBalance-BET_PER_GAME;
+		}
 	}
-	
-	public static void main(String[] args) {
-		System.out.println("Welcome to gambler simulator");
-		int stake = INITIAL_STAKE,numOfDays=20,totalAmount=0;
-		stake = playGame(stake);
-		for (int day = 1; day <= numOfDays; day++) {
-			stake = INITIAL_STAKE;
-			boolean continueToPlay = terminalCondition(stake);
-			while(continueToPlay == true) {
-				double winProbability = Math.random();
-				if(winProbability < 0.5) {
-					System.out.println("Player has lost the bet");
-					stake=stake-1;
-				}
-				else {
-					System.out.println("Player has won the bet");
-					stake=stake+1;
-				}
-				System.out.println("current stake value is: "+stake);
+	public static void gameForMonth() 
+	{
+		int noOfDays=30;
+		int amountInADay=0;
+		int totalAmount=0;
+		for(int day=1; day<=noOfDays;day++) 
+		{
+			System.out.println();
+			System.out.print("Day "+day);
+			amountInADay=gameResign(TOTAL_STAKE);
+			totalAmount+=amountInADay; 
+			if(amountInADay==TOTAL_STAKE+0.5*TOTAL_STAKE) 
+			{
+				System.out.println(" Won By:"+(int)(TOTAL_STAKE*1.5));
 			}
-			totalAmount=totalAmount+stake;
+			else 
+			{
+				System.out.println(" Lost By:"+(int)(0.5*TOTAL_STAKE));
+			}
+
+
 		}
-		System.out.println("stake at the end of 20 days: "+totalAmount);
+		
 
 	}
+	public static int gameResign(int availableBalance) 
+	{
+		while(availableBalance<TOTAL_STAKE*1.5 && availableBalance>0.5*TOTAL_STAKE) 
+		{
+			availableBalance=playGame(availableBalance);
 
+
+		}
+		return availableBalance;
+
+	}
+	public static int gamePlayForDays(int noOfDays, int availableBalance) 
+	{
+		int totalAmount=availableBalance;
+		int amountInOneDay=0;
+		for(int day=1; day<=noOfDays;day++) 
+		{
+			amountInOneDay=gameResign(TOTAL_STAKE);
+			totalAmount+=amountInOneDay;
+		}
+		return totalAmount;
+
+	}
+		
 }
